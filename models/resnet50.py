6b9aa42568d56d250f287137b1c0040b01ffd14b
Hariton <pitsikhariton@gmail.com>
2025-11-04 17:34:41 +0400

    feat: resnet50 architecture

--- models/resnet50.py
+++ models/resnet50.py
@@ -31,10 +31,10 @@ class BottleneckGenerator(nn.Module):
             block = nn.Sequential(
             nn.Conv2d(in_channels, self.shape1, kernel_size=1, bias=False),
             nn.BatchNorm2d(self.shape1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(self.shape1, self.shape2, kernel_size=3, padding=1, stride=curr_stride, bias=False),
             nn.BatchNorm2d(self.shape2),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(self.shape2, self.shape3, kernel_size=1, bias=False),
             nn.BatchNorm2d(self.shape3),
             )
@@ -53,5 +53,62 @@ class BottleneckGenerator(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.ones_(m.weight)
                 nn.init.zeros_(m.bias)
+
 class ResNet(nn.Module):
-    pass
+    """
+    Actual ResNet50.
+
+    Args:
+        in_channels - shape of input for this model
+    """
+    def __init__(self, in_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.conv0 = nn.Sequential(
+            nn.Conv2d(self.in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.conv0.apply(self.init_conv0_weights)
+        
+        self.conv1 = BottleneckGenerator(3, 64, 64, 64, 256, 1)
+        self.conv2 = BottleneckGenerator(4, 256, 128, 128, 512, 2)
+        self.conv3 = BottleneckGenerator(6, 512, 256, 256, 1024, 2)
+        self.conv4 = BottleneckGenerator(3, 1024, 512, 512, 2048, 1)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(2048, 1000)
+
+    def _init_conv0_weights(self, m):
+        """
+        He (Kaiming) weight initialization for stem layer of ResNet50
+        """
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        """
+        Forward-propagation of resnet
+        """
+        x = self.conv0(x)
+        for block in self.conv1.blocks:
+            x = block(x)  
+        
+        for block in self.conv2.blocks:
+            x = block(x) 
+        
+        for block in self.conv3.blocks:
+            x = block(x)  
+        
+        for block in self.conv4.blocks:
+            x = block(x)  
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
