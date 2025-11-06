46e50f0a70670cc3536c7fbcc9a4da701b43d394
Hariton <pitsikhariton@gmail.com>
2025-11-06 21:56:04 +0400

    feat: early stopping mechanism

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,32 @@
 import pickle
+import copy
 import torch
 import numpy as np
+
+class EarlyStopping:
+    """
+    Class for early stopping implementation
+    """
+    def __init__(self, patience=5, epsilon=.005, verbose=False):
+        self.patience = patience
+        self.verbose = verbose
+        self.epsilon = epsilon
+
+        self.best_score = 100
+        self.curr_patience = 0
+        self.curr_model_state = None
+
+    def step(self, curr_score, model):
+        if curr_score >= self.best_score - self.epsilon:
+            self.curr_patience += 1
+            if self.curr_patience >= self.patience:
+                return False, self.curr_model_state
+        else:
+            self.curr_patience = 0
+            self.best_score = curr_score
+            self.curr_model_state = copy.deepcopy(model.state_dict())
+            return True, None
+
 
 def load_cifar100_file(filename):
     with open(filename, 'rb') as f:
