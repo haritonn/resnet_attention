7a578b549361881eeed61170905b92a4487caf71
Hariton <pitsikhariton@gmail.com>
2025-11-06 22:18:21 +0400

    feat: config file

---
+++ config/config.py
@@ -0,0 +1,26 @@
+import torch
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Directories
+DATA_DIR = './data'
+CHECKPOINTS_DIR = './checkpoints' # Checkpoints of trained model
+TRAIN_RESULTS_DIR = './train_results' # Plots of train process
+
+# Core parameters
+EARLY_STOPPING_PATIENCE = 7
+USING_AUGMENTATIONS = True
+ATTENTION_REDUCTION = 16 # For channel attention mechanism
+
+# Train parameters
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-3
+NUM_EPOCHS = 200
+CRITERION = 'CrossEntropyLoss' # Using getattr to parse, so write like name in PyTorch module
+
+# Other
+RANDOM_SEED = 42
+WEIGHT_DECAY = 2e-4 # L2
+VAL_SPLIT = .2
+NUM_CLASSES = 100 # CIFAR-100
+SGD_MOMENTUM = .9 # In case of using SGD (current approach)
